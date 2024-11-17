b4cfa835efadf2aefa1a08fa712ca23c88cb661c
neezar <rforlyffe@gmail.com>
2024-11-17 22:19:36 +0800

    add styles

--- app/root.tsx
+++ app/root.tsx
@@ -6,8 +6,10 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+import styles from "./tailwind.css?url";
 
 import "./tailwind.css";
+import { Button } from "./components/ui/button";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -18,7 +20,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: styles,
   },
 ];
 
